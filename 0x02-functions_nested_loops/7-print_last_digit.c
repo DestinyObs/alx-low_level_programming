dc1a419f0b7ff43699b51c74987254358bfd5373
DestinyObs <obueh.destiny@lmu.edu.ng>
2022-10-19 03:38:00 -0700

    me8

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit of the given vakue
+ * @value: the given value
+ * Return: returns 0 if succcessful 1 if otherwise
+ * done by ojile samuel
+ */
+
+int print_last_digit(int value)
+{
+	int remain = value % 10;
+
+	if (remain < 0)
+	{
+		remain = -(remain);
+		_putchar(remain + '0');
+		return (remain);
+	}
+	else
+	{
+		_putchar(remain + '0');
+		return (remain);
+	}
+	return (0);
+}
